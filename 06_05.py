149d67487fdde98def18cd16c611c4252f32649a
Mstsislau <mr.chadway@gmail.com>
2023-10-03 13:42:13 +0300

    definition  restruction

--- 06_05.py
+++ 06_05.py
@@ -3,11 +3,16 @@
 
 number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-start_point = 0
-end_point = len(number_list) - 1
-while start_point < end_point:
-    number_list[start_point], number_list[end_point] = number_list[end_point], number_list[start_point]
-    start_point += 1
-    end_point -= 1
 
+def number_reversed(number_list):
+    start_point = 0
+    end_point = len(number_list) - 1
+    while start_point < end_point:
+        number_list[start_point], number_list[end_point] = number_list[end_point], number_list[start_point]
+        start_point += 1
+        end_point -= 1
+        return number_list
+
+
+number_reversed(number_list)
 print(number_list)
